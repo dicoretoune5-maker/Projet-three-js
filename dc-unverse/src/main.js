334f344403e552a517ae6a5982b62d19b64226fb
dicoretoune5-maker <dicoretoune5@gmail.com>
2026-05-01 16:36:47 +0200

    angle de vue

--- dc-unverse/src/main.js
+++ dc-unverse/src/main.js
@@ -2,4 +2,5 @@ import * as THREE from 'three';
 // Création de l'univers (la scène)
 const scene = new THREE.Scene();
 // Création de la caméra
-
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
