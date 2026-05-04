ec875cbda9d2409e106b61a826912f2fcf17e50c
dicoretoune5-maker <dicoretoune5@gmail.com>
2026-05-04 09:07:10 +0200

    avance les étoiles

--- dc-unverse/src/main.js
+++ dc-unverse/src/main.js
@@ -47,9 +47,12 @@ function animate() {
   // On parcourt les étoiles stockées
   for (let i=0; i < starArray.length; i++) {
     const star = starArray[i];
+    star.position.z += 0.5; // Avance les étoiles vers la caméra
     // Si l'étoile dépasse la caméra, on la replace loin derrère
     if (star.position.z > 50) {
       star.position.z = -150; // Repositionner loin derrière
+    }
+  }
 
   renderer.render(scene, camera);
 }
